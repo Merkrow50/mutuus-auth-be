43793ae7fbcbde1062b89fc3e16bf5e1eeed5bdb
marcelo junior <marcelo.junior@cflow.com.br>
2023-06-08 14:01:14 -0300

    criando jwt authentication com user roles

---
+++ src/main/java/com/mutuus/MutuusApplication.java
@@ -0,0 +1,46 @@
+package com.mutuus;
+
+
+import com.mutuus.auth.AuthenticationService;
+import com.mutuus.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import static com.mutuus.user.Role.ADMIN;
+import static com.mutuus.user.Role.MANAGER;
+
+
+@SpringBootApplication
+public class MutuusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutuusApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
+}
---
+++ src/main/java/com/mutuus/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.mutuus.token;
+
+public enum TokenType {
+  BEARER
+}
